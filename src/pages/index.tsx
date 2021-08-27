02ba992a676204ef95f4e9f80697b382f4d73ade
richardsonbruno <richardsonbrc@gmail.com>
2021-08-27 12:36:05 -0300

    adicionado o revalidate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ export const getStaticProps = async () => {
   const { places } = await client.request<GetPlacesQuery>(GET_PLACES)
 
   return {
+    revalidate: 30,
     props: {
       places
     }
